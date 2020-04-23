eb1a989ef03ffec883fa97bd184237060bea8a86
yyf <2381510397@qq.com>
2020-04-23 16:38:28 +0800

    修改了my_information.js中的文件增加了从数据库读取学院信息

--- pages/my_information/my_information.js
+++ pages/my_information/my_information.js
@@ -1,8 +1,13 @@
+
+import {
+  $get
+} from "../../utils/requestbasic.js"
+
 const app = getApp();
 Page({
   data: {
     picker: ['学生', '老师'],
-    picker1: ['计算机与信息工程学院', '动物科学学院', '兽医学院', '农学院', '林学院', '草原与资源环境学院', '沙漠治理学院', '机电工程学院', '水利与土木建筑学院', '材料科学与艺术学院', '积极管理学院', '食品科学与工程学院', '生命科学学院', '人文社会科学学院', '外国语学院',            ],
+    picker1: [],
   },
   PickerChange(e) {
     console.log(e);
@@ -16,4 +21,19 @@ Page({
       index_xueyuan: e.detail.value
     })
   },
+  onLoad: function (options) {
+    let xueyaun = this.GetXueYuan()
+    
+  },
+
+  async GetXueYuan(){
+    let {
+      classes
+    } = await $get("https://spergol.com/selclasses")
+    let name = classes.map(r => r.name)
+    console.log(name)
+    this.setData({
+      picker1:name
+    })
+  }
 })
